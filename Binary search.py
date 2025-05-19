aabcadf30ac7e11d5b9585c4e87bcdcd94bf279d
Joshua Thomas <joshuathomasjohnson.231me322@nitk.edu.in>
2025-05-19 21:21:01 +0530

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -1,2 +1,22 @@
 #FOR WEEK 2 OF DSA 
 #SOLVE ALL QUESTIONS UNDER THIS TOPIC HERE
+#find number desired
+#Approach
+ # 1. use mid
+ # 2. move left or right acc to the target
+
+def find_target(self,nums,target):
+  n=len(nums)
+  l,r=0,n-1
+  while l<=r:
+    mid=l+(r-l)//2
+    if nums[mid]>target:
+      r=mid-1
+    elif nums[mid]<target:
+      l=mid+1
+    else:
+      return mid
+  return -1
+
+
+    
