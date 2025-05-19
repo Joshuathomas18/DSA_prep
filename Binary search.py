360a56e595dcae9b83c3b25c19266207567291a1
Joshua Thomas <joshuathomasjohnson.231me322@nitk.edu.in>
2025-05-19 22:56:01 +0530

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -18,5 +18,32 @@ def find_target(self,nums,target):
       return mid
   return -1
 
+#2D binary search
 
-    
+#FIRST FIND ROW
+#THEN BINARY SEARCH IN COL
+
+def find_numer(self,matrix,target):
+ 
+ 
+#KOKO BANANAS
+#use hours binary search(max(list) and 1)
+#apply if-else
+
+def find_hours(self,nums,h):
+ 
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
+            if totalTime <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+        return res
